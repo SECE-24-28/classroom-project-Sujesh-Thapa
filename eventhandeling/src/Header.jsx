90f63d498c6392b7297f4e919e1bae0d5db484a8
Sujesh-Thapa <sujeshthapa06@gmail.com>
2025-12-08 12:04:28 +0530

    basics along with event handeling

---
+++ eventhandeling/src/Header.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+const Header = () => {
+    let des = { background: "black", color: "white", padding: "10px" }
+
+    return (
+        <div>
+            <h1 style={des}>Nepal</h1>
+            <p>Nepal is a landlocked country in South Asia, nestled between India to the south and China to the north. Known for its breathtaking natural beauty and extraordinary geographical diversity, Nepal stretches from lowland tropical plains to some of the world’s highest mountains. The country is home to the Himalayan range, including Mount Everest, the tallest peak on Earth. Its landscapes include lush forests, deep valleys, winding rivers, and high-altitude deserts, making Nepal one of the most geographically varied nations in the world.</p>
+
+        </div>
+    )
+}
+
+export default Header   
--- first_project/src/App.jsx
+++ first_project/src/App.jsx
@@ -4,11 +4,16 @@ import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  let city="Kathmandu"
+  let nums=[1,2,3,4,5]
 
   return (
     <>
-      <h1>Vite + React</h1>
+      <p>I am from {city}</p>
+      <p>Lucky Numbers: {[10, 20, 30]}</p>
+      <p>Numbers: {nums}</p>
+      <p>{true}</p>
+      <p>{JSON.stringify({name:"Sujesh",age:21})}</p>
     </>
   )
 }
